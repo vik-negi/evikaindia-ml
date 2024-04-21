b3ea688e984451e5db5edde4c07b0e96b358208d
vik-negi <vikramnegi175@gmail.com>
2024-04-21 14:35:18 +0530

    remove corsheaders from installed app

--- evika/settings.py
+++ evika/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
-    # "corsheaders",
     "evika",
 ]
 
@@ -50,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 REST_FRAMEWORK = {
